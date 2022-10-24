6bf56a9062fb7d6cb2bee86521ba6de172ed5606
Chuks Enedeh <c.enedeh@televic.com>
2022-10-24 10:17:56 +0100

    Add console app for testing

---
+++ NonFungibleTokenMetaDataExtraction.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using NonFungibleTokenMetaDataExtraction.Application.Model;
+using NonFungibleTokenMetaDataExtraction.Application.Services;
+
+var client = NonFungibleTokenClient.GetNonFungibleTokenClient("4441cc2988c64bc1bc3029f55d39dbe6");
+
+while (true)
+{
+    Console.Write("Please enter contract address: ");
+    var contract = Console.ReadLine();
+    Console.Write("Please enter index number: ");
+    var index = Console.ReadLine();
+    var request = new NonFungibleTokenInputRequest()
+    {
+        ContractAddress = contract,
+        TokenIndeX = index
+    };
+
+    var model = await client.GetNonFungibleTokenModel(request);
+    Console.WriteLine(model);
+    Console.ReadLine();
+}
