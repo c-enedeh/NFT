c19698c21ad23c274f9597a3dadd1c7d90c440f0
Chuks Enedeh <c.enedeh@televic.com>
2022-10-24 10:05:21 +0100

    Expose endpoint

---
+++ NonFungibleTokenMetaDataExtraction.Api/Controllers/NFTValuationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NonFungibleTokenMetaDataExtraction.Api.Model;
+using NonFungibleTokenMetaDataExtraction.Api.Services;
+
+namespace NonFungibleTokenMetaDataExtraction.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NFTValuationsController : ControllerBase
+    {
+        private readonly INonFungibleTokenValuations _nonFungibleTokenValuations;
+
+        public NFTValuationsController(ILogger<NFTValuationsController> logger, INonFungibleTokenValuations nonFungibleTokenValuations)
+        {
+            _nonFungibleTokenValuations = nonFungibleTokenValuations;
+        }
+
+        [HttpPost]
+        public IActionResult GetMetaData([FromBody] Request model)
+        {
+            var response = _nonFungibleTokenValuations.GetMetaData(model);
+            return Ok(response);
+        }
+    }
+}
